c32af5b7c8fd72a4f5f3123449c8e09e474d9567
Nikita_Stalintsev <nikitastalintsev@gmail.com>
2025-11-21 01:20:04 +0500

    Update nav block in base.html

--- django_site/portfolio/views.py
+++ django_site/portfolio/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 
+nav_buttons = [
+    {'title': 'ОБО МНЕ', 'url_name': 'about', 'path': '/'},
+    {'title': 'НАВЫКИ', 'url_name': 'skills', 'path': '/skills/'},
+    {'title': 'ПРОЕКТЫ', 'url_name': 'projects', 'path': '/projects/'},
+    {'title': 'КОНТАКТЫ', 'url_name': 'contacts', 'path': '/contacts/'},
+]
+context = {
+    'nav_buttons': nav_buttons,
+}
+
 def about(request):
-    return render(request, 'portfolio/about.html')
+    return render(request, 'portfolio/about.html', context=context)
 
 def skills(request):
-    return render(request, 'portfolio/skills.html')
+    return render(request, 'portfolio/skills.html', context=context)
 
 def projects(request):
-    return render(request, 'portfolio/projects.html')
+    return render(request, 'portfolio/projects.html', context=context)
 
 def contacts(request):
-    return render(request, 'portfolio/contacts.html')
+    return render(request, 'portfolio/contacts.html', context=context)
