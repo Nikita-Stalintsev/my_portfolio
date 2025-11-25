19de8f04e8eacd86a54bb5f90776505675f3b7cc
Nikita_Stalintsev <nikitastalintsev@gmail.com>
2025-11-25 21:19:38 +0500

    Renamed contacts in contact

--- django_site/portfolio/context_processors.py
+++ django_site/portfolio/context_processors.py
@@ -4,6 +4,6 @@ def nav_buttons(request):
             {'title': 'ОБО МНЕ', 'url_name': 'about', 'path': '/'},
             {'title': 'НАВЫКИ', 'url_name': 'skills', 'path': '/skills/'},
             {'title': 'ПРОЕКТЫ', 'url_name': 'projects', 'path': '/projects/'},
-            {'title': 'КОНТАКТЫ', 'url_name': 'contacts', 'path': '/contacts/'},
+            {'title': 'КОНТАКТЫ', 'url_name': 'contact', 'path': '/contact/'},
         ]
     }
