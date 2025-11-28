c649c21fff95bb513f1393a7bc2312859449b301
Nikita_Stalintsev <nikitastalintsev@gmail.com>
2025-11-28 23:14:00 +0500

    Switching def on class

--- django_site/portfolio/views.py
+++ django_site/portfolio/views.py
@@ -1,28 +1,51 @@
-from django.shortcuts import render, redirect
+# from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView
+
 from .models import *
 from .forms import *
 
 
-def about(request):
-    return render(request, 'portfolio/about.html', )
+class AboutView(TemplateView):
+    template_name = 'portfolio/about.html'
 
 
-def skills(request):
-    skills = Skill.objects.all()
-    return render(request, 'portfolio/skills.html', context={'skills': skills})
+# def about(request):
+#     return render(request, 'portfolio/about.html', )
 
 
-def projects(request):
-    projects = Project.objects.all()
-    return render(request, 'portfolio/projects.html', context={'projects': projects})
+class SkillsListView(ListView):
+    model = Skill
+    template_name = 'portfolio/skills.html'
+    context_object_name = 'skills'
 
 
-def contact(request):
-    if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('about')
-    else:
-        form = ContactForm()
-    return render(request, 'portfolio/contact.html', context={'form': form})
+# def skills(request):
+#     skills = Skill.objects.all()
+#     return render(request, 'portfolio/skills.html', context={'skills': skills})
+
+
+class ProjectsListView(ListView):
+    model = Project
+    template_name = 'portfolio/projects.html'
+    context_object_name = 'projects'
+
+
+# def projects(request):
+#     projects = Project.objects.all()
+#     return render(request, 'portfolio/projects.html', context={'projects': projects})
+
+class ContactCreateView(CreateView):
+    form_class = ContactForm
+    template_name = 'portfolio/contact.html'
+    success_url = reverse_lazy('about')
+
+# def contact(request):
+#     if request.method == 'POST':
+#         form = ContactForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('about')
+#     else:
+#         form = ContactForm()
+#     return render(request, 'portfolio/contact.html', context={'form': form})
