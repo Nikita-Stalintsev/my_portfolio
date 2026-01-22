b273380a906aa1467bc100b97ed943f37048b859
Nikita-Stalintsev <nikitastalintsev@gmail.com>
2026-01-22 09:42:22 +0500

    TIK-006: Fix imports and django debug settings

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-hcke9&&wy8uo11ky89)wwf@y6*#ppl$=s@h^069g^6r33^7bqi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 INTERNAL_IPS = ["127.0.0.1",]
 
 # Application definition
@@ -25,8 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio.apps.PortfolioConfig',
-    'debug_toolbar',
-]
+] + (['debug_toolbar'] if DEBUG else [])
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -36,8 +36,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-]
+] + (['debug_toolbar.middleware.DebugToolbarMiddleware'] if DEBUG else [])
 
 ROOT_URLCONF = 'django_site.urls'
 
--- django_site/portfolio/admin.py
+++ django_site/portfolio/admin.py
@@ -4,15 +4,15 @@ from .models import *
 
 @admin.register(Skill)
 class SkillAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content')
-    list_display_links = ('id', 'title', 'content')
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
     ordering = ('id', )
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content')
-    list_display_links = ('id', 'title', 'content')
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
     ordering = ('id', )
 
 @admin.register(ProjectTag)
